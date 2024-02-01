2cbbe6573f5bc55541e48bf9fafe593f9a936619
Mukul Sharma <mukulsharma8431@gmail.com>
2024-02-01 16:57:00 +0530

    styling of container component done

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
 const isMenuOpen = useSelector((store)=> store.app.isMenuOpen);
 
   return (
-    <div className={'fixed top-0 w-screen h-screen sm:h-14 bg-slate-400 flex flex-col sm:flex-row sm:justify-between items-center' + (!isMenuOpen ? " h-14" : " z-10")}>
+    <div className={'fixed top-0 w-screen h-screen sm:h-14 bg-slate-400 flex flex-col sm:flex-row sm:justify-between items-center z-50' + (!isMenuOpen ? " h-14" : "")}>
             <Link className="flex items-center">
                 <img src={logo} alt="best cart logo" className='w-12 h-12 ml-2' />
                 <span className='ml-2 font-extrabold text-2xl'>BEST STORE</span>
