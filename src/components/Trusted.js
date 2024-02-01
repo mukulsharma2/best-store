4436532d3c7dc13d0d6d3abab1cef7bb2b9418c5
Mukul Sharma <mukulsharma8431@gmail.com>
2024-02-01 17:51:40 +0530

    trusted component done

--- src/components/Trusted.js
+++ src/components/Trusted.js
@@ -2,9 +2,41 @@ import React from 'react'
 
 const Trusted = () => {
   return (
-    <div>
-      trusted
-    </div>
+      <div className="">
+        <h3>Trusted By 1000+ Companies</h3>
+        <div className="">
+          <div className="">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image2.png"
+              alt="trusted-brands"
+            />
+          </div>
+          <div className="">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image3.png"
+              alt="trusted-brands"
+            />
+          </div>
+          <div className="">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image4.png"
+              alt="trusted-brands"
+            />
+          </div>
+          <div className="">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image6.png"
+              alt="trusted-brands"
+            />
+          </div>
+          <div className="">
+            <img
+              src="https://raw.githubusercontent.com/solodev/infinite-logo-carousel/master/images/image8.png"
+              alt="trusted-brands"
+            />
+          </div>
+        </div>
+      </div>
   )
 }
 
