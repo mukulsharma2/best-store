c4304994ecb6f622c546b6fa8e28f61d436fb9f5
Mukul Sharma <mukulsharma8431@gmail.com>
2024-02-06 01:45:18 +0530

    search filter and category filter completed

--- src/components/FilterSection.js
+++ src/components/FilterSection.js
@@ -1,9 +1,145 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {formatPrice} from '../helper/constants'
+import { FaCheck } from "react-icons/fa";
+import { useDispatch, useSelector } from 'react-redux';
+import { filterProducts } from '../store/productSlice';
 
 const FilterSection = () => {
+  const dispatch = useDispatch()
+  const data = useSelector(store=>store.app.data)
+  const filters = useSelector(store=>store.product.filters)
+
+const getParticularData = (data, property)=>{
+let particularData = data.map((currEle)=>{
+  return currEle[property]
+})
+return (particularData = ['All', ...new Set(particularData)])
+}
+
+const categoryData = getParticularData(data, 'category')
+
+useEffect(()=>{
+  
+},[])
+
   return (
     <div>
-      filter
+      <div className="">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            name="text"
+            placeholder="Search"
+            onChange={(e)=>{
+              dispatch(filterProducts({
+                name: e.target.name,
+                value: e.target.value,
+                fullData: data,
+              }))
+            }}
+          />
+        </form>
+      </div>
+
+       <div className="">
+        <h3>Category</h3>
+        <div>
+          {categoryData && categoryData.map((curElem, index) => {
+            return (
+              <button
+                key={index}
+                type="button"
+                name="category"
+                value={curElem}
+                className={curElem === filters.category ? "text-blue-600 border-b" : ""}
+                onClick={(e)=>{
+                  dispatch(filterProducts({
+                    name: e.target.name,
+                    value: e.target.value,
+                    fullData: data,
+                  }))
+                }}>
+                {curElem}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* <div className="">
+        <h3>Company</h3>
+
+        <form action="#">
+          <select
+            name="company"
+            id="company"
+            className=""
+            onClick={updateFilterValue}>
+            {companyData.map((curElem, index) => {
+              return (
+                <option key={index} value={curElem} name="company">
+                  {curElem}
+                </option>
+              );
+            })}
+          </select>
+        </form>
+      </div>
+
+      <div className="">
+        <h3>Colors</h3>
+
+        <div className="">
+          {colorsData.map((curColor, index) => {
+            if (curColor === "all") {
+              return (
+                <button
+                  key={index}
+                  type="button"
+                  value={curColor}
+                  name="color"
+                  className=""
+                  onClick={updateFilterValue}>
+                  all
+                </button>
+              );
+            }
+            return (
+              <button
+                key={index}
+                type="button"
+                value={curColor}
+                name="color"
+                style={{ backgroundColor: curColor }}
+                className={color === curColor ? "btnStyle active" : "btnStyle"}
+                onClick={updateFilterValue}>
+                {color === curColor ? <FaCheck className="checkStyle" /> : null}
+              </button>
+            );
+          })}
+        </div>
+      </div>
+
+      <div className="">
+        <h3>Price</h3>
+        <p>
+          {formatPrice(price)}
+        </p>
+        <input
+          type="range"
+          name="price"
+          min={minPrice}
+          max={maxPrice}
+          value={price}
+          onChange={updateFilterValue}
+        />
+      </div>
+
+      <div className="">
+        <button className="" onClick={clearFilters}>
+          Clear Filters
+        </button>
+      </div> */}
     </div>
   )
 }
--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -5,6 +5,13 @@ const productSlice = createSlice({
     initialState: {
         showGridView: true,
         sortedData: [],
+        filters: {
+            text: "",
+            category: "",
+            company: "",
+            color: "",
+            price: 0,
+        },
     },
     reducers: {
         setGridView: (state)=>{
@@ -22,11 +29,73 @@ const productSlice = createSlice({
                 if (action.payload === "highest")return b.price - a.price;
                 if (action.payload === "a-z") return a.name.localeCompare(b.name);
                 if (action.payload === "z-a")return  b.name.localeCompare(a.name);
-              }
+            }
             state.sortedData.sort(fn);
+        },
+        filterProducts: (state, action)=>{
+            state.filters = {
+                ...state.filters,
+                [action.payload.name]: action.payload.value
+            }
+
+            state.sortedData = action.payload.fullData
+
+      const { text, category, company, color, price } = state.filters;
+
+      if (text) {
+        state.sortedData = state.sortedData.filter(curElem => curElem.name.toLowerCase().includes(text.toLowerCase()));
+      }
+
+      if (category !== "all") {
+        state.sortedData = state.sortedData.filter(curElem => curElem.category === category);
+      }
+
+    //   if (company !== "all") {
+    //     state.sortedData = state.sortedData.filter(
+    //       (curElem) => curElem.company.toLowerCase() === company.toLowerCase()
+    //     );
+    //   }
+
+    //   if (color !== "all") {
+    //     state.sortedData = state.sortedData.filter((curElem) =>
+    //       curElem.colors.includes(color)
+    //     );
+    //   }
+
+    //   if (price === 0) {
+    //     state.sortedData = state.sortedData.filter(
+    //       (curElem) => curElem.price == price
+    //     );
+    //   } else {
+    //     state.sortedData = state.sortedData.filter(
+    //       (curElem) => curElem.price <= price
+    //     );
+    //   }
+    //   return {
+    //     ...state,
+    //     filter_products: state.sortedData,
+    //   };
+
+    // case "CLEAR_FILTERS":
+    //   return {
+    //     ...state,
+    //     filters: {
+    //       ...state.filters,
+    //       text: "",
+    //       category: "all",
+    //       company: "all",
+    //       color: "all",
+    //       maxPrice: 0,
+    //       price: state.filters.maxPrice,
+    //       minPrice: state.filters.maxPrice,
+    //     },
+    //   };
+
+
+
         },
     },
 })
 
 export default productSlice.reducer
-export const {setGridView, setListView, setSortedData, sortProducts} =  productSlice.actions
+export const {setGridView, setListView, setSortedData, sortProducts, filterProducts} =  productSlice.actions
